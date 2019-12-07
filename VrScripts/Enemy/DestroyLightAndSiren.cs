aecb83bc3506ff355f15cb84b68c77e3829a5d00
Q5124634 <33085482+Q5124634@users.noreply.github.com>
2019-12-07 20:25:36 +0000

    VrCodeSamples
    These are all the code samples used on my virtual reality project in my final year, a lot went into the creation of this project and its something i am very proud of.

---
+++ VrScripts/Enemy/DestroyLightAndSiren.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyLightAndSiren : MonoBehaviour
+{
+    public GameObject[] EnemiesArray;
+    public AudioClip WindDown;
+    public AudioSource myAudioSource;
+    bool EnemiesAlive = true;
+
+    void FixedUpdate()
+    {
+        EnemiesAlive = false;
+
+        for (int i = 0; i < EnemiesArray.Length; i++)
+        {
+            if (EnemiesArray[i] != null)
+            {
+                EnemiesAlive = true;
+
+            }
+        }
+
+        if (EnemiesAlive == false)
+        {
+            myAudioSource.PlayOneShot(WindDown);
+            Destroy(gameObject);
+            ScoreSystem.AddScore(100);
+        }
+    }
+}
---
+++ VrScripts/RWVR_ControllerManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RWVR_ControllerManager : MonoBehaviour {
+
+    public static RWVR_ControllerManager Instance; // 1
+    public GameObject Gun1;
+    public GameObject Gun2;
+    public GameObject Gun3;
+    public GameObject Gun4;
+    public GameObject Gun5;
+    public GameObject Gun6;
+    public GameObject Gun7;
+    public GameObject Gun8;
+    public GameObject Gun9;
+    public GameObject Gun10;
+    public GameObject Gun11;
+    public RWVR_InteractionController leftController; // 2
+    public RWVR_InteractionController rightController; // 3
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public bool AnyControllerIsInteractingWith<T>() // 1
+    {
+        if (leftController.InteractionObject && leftController.InteractionObject.GetComponent<T>() != null) // 2
+        {
+            return true;
+        }
+
+        if (rightController.InteractionObject && rightController.InteractionObject.GetComponent<T>() != null) // 3
+        {
+            return true;
+        }
+
+        return false; // 4
+    }
+
+}
